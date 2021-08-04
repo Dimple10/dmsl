0f88a670b36d8e2c262e7f93395d8af8a7cfd630
Kris Pardo <kpardo@caltech.edu>
2021-08-04 16:38:12 -0700

    Allowed for two limits for PS mass case.

--- drivers/make_tab_results.py
+++ drivers/make_tab_results.py
@@ -35,18 +35,26 @@ def get_chains(lenstypes, surveys):
                 chains[tstring] = dill.load(buff)
     return chains
 
-def get_nums_for_table(chain, percent=90, twosided=True, side=None):
+def get_nums_for_table(chain, percent=95, twosided=False, side=None):
     ave = np.average(chain)
     if twosided:
         confup = np.percentile(chain, 100-(100-percent)/2.)
         confdown = np.percentile(chain, (100-percent)/2.)
-    return ave, confup, confdown
+        conf = [ave, confup, confdown]
+        sgn = -1
+    if twosided == False and side == 'up':
+        conf = np.percentile(chain, percent)
+        sgn = 1
+    if twosided == False and side == 'down':
+        conf = np.percentile(chain, 100-percent)
+        sgn = 0
+    return conf, sgn
 
 def write_table(data, firstcol, cols):
     df = pd.DataFrame(data=data, columns=cols[1:])
     df.insert(loc=0, column=cols[0], value=firstcol)
     df.to_latex(PAPERDIR+'tab_results.tex', index=False,
-               column_format='l|cccc', label='tab:results', caption='Results',
+               column_format='l|cccc', label='tab:results', caption='Forecasted 95\% Limits on DM Substructure Using the Roman Space Telescope EML Survey',
                escape=False,
                bold_rows=True)
     return df
@@ -65,21 +73,33 @@ counter = 0
 for l in lenstypes:
     kstrings = [f'{l}', f'{l}_frac']
     for kstring in kstrings:
-        nums = np.empty((3, 4))
+        nums = np.empty((2, 4))
         nums[:] = np.nan
         for i in range(0,np.shape(chains['Roman_'+kstring])[1]):
             coli = i
+            side = 'up'
+            twosided = False
             if ('frac' in kstring) and (i == np.shape(chains['Roman_'+kstring])[1] - 1):
                 coli = -1
+            if (l=='gaussian') and(i==1):
+                side = 'down'
             if (l=='nfw') and (i==1):
                 coli = 2
-            nums[:,coli] = get_nums_for_table(chains['Roman_'+kstring][:,i])
+            nums[:,coli] = get_nums_for_table(chains['Roman_'+kstring][:,i], side=side, twosided = twosided)
         info = []
         for i in range(0, np.shape(nums)[1]):
             if np.all(np.isnan(nums[:,i])):
                 info.append('--')
+            elif (l=='ps') and (i==0):
+                upconf, _ = get_nums_for_table(chains['Roman_'+kstring][:,i], side='up')
+                downconf, _ = get_nums_for_table(chains['Roman_'+kstring][:,i], side='down')
+                info.append(f'$>{downconf:.1f}~\& <{upconf:.1f}$')
             else:
-                info.append(f'${nums[0,i]:.2f}^{{{nums[1,i]:.1f}}}_{{{nums[2,i]:.1f}}}$')
+                if nums[1,i] == 1:
+                    sgn = '<'
+                if nums[1,i] == 0:
+                    sgn = '>'
+                info.append(f'${sgn}{nums[0,i]:.2f}$')
         d[counter] = info
         counter +=1
 write_table(d, nicelenstypes, colnames)
@@ -93,4 +113,5 @@ text = text.replace("\\end{table}", "\\end{table*}")
 # Write the file out again
 with open(PAPERDIR+'tab_results.tex', "w") as f:
   f.write(text)
-
+with open(FINALDIR+'tab_results.tex', "w") as f:
+    f.write(text)
