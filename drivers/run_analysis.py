2a4a074eb60fd26e3c361f98e53c2ba60f75d609
Dimple <sarnaaik@usc.edu>
2024-07-03 11:57:09 -0700

    Cluster update 2" q knfck ::

--- drivers/run_analysis.py
+++ drivers/run_analysis.py
@@ -14,6 +14,7 @@ nfw = NFW(**nfwprops)
 nn = 1e3
 cdm = CDM_Test(m_l = np.logspace(0,6,100))
 press_s=PressSchechter_test(m_l=np.logspace(0,6,100))
+tinker = Tinker(m_l=np.logspace(0,6,100))
 kepler = s.Kepler()
 roman = s.Roman(alphasigma = 0.1*u.uas/u.yr**2*(nn/1.e8)**(1./2.))
 wfirstlike = s.WFIRSTLike(alphasigma = 0.1*u.uas/u.yr**2*(nn/(1.e11*0.05*4*np.pi)))
@@ -52,15 +53,21 @@ wfirstlike = s.WFIRSTLike(alphasigma = 0.1*u.uas/u.yr**2*(nn/(1.e11*0.05*4*np.pi
 
 #time1 = datetime.datetime.now()
 #print("Running CDM + nfw case, no fraction")
-#s = Sampler(nstars=int(nn), ntune=int(1e3), nsamples=int(1e3), survey=roman,
-#        MassProfile=nfw, MassFunction=cdm, nchains=16, ndims=2)
+#s = Sampler(nstars=int(nn), ntune=int(1e3), nsamples=int(1e2), survey=roman,
+#        MassProfile=nfw, MassFunction=cdm, nchains=8, ndims=2)
 #print('This took',str(datetime.datetime.now()-time1))
 
 time1 = datetime.datetime.now()
-print("Running Press_S+ nfw case, no fraction")
-s = Sampler(nstars=int(nn), ntune=int(1e3), nsamples=int(1e4), survey=roman,
-        MassProfile=nfw, MassFunction=press_s, nchains=128, ndims=2)
+print("Running Tinker + nfw case, no fraction")
+s = Sampler(nstars=int(nn), ntune=int(1e3), nsamples=int(1e2), survey=roman,
+        MassProfile=nfw, MassFunction=tinker, nchains=8, ndims=2)
 print('This took',str(datetime.datetime.now()-time1))
+
+#time1 = datetime.datetime.now()
+#print("Running Press_S+ nfw case, no fraction")
+#s = Sampler(nstars=int(nn), ntune=int(1e3), nsamples=int(1e4), survey=roman,
+#        MassProfile=nfw, MassFunction=press_s, nchains=128, ndims=2)
+#print('This took',str(datetime.datetime.now()-time1))
 
 #time1 = datetime.datetime.now()
 #print("Running NFW case, no fraction")
